f4b4101d3b74e49f3bf586922607e90510da3a3f
Marcos150 <marcosruizrubio@gmail.com>
2024-04-22 14:59:01 +0200

    Agregado script sql y actualizado spring.

---
+++ script.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE proyecto_spring;
+
+CREATE TABLE IF NOT EXISTS trabajador
+(
+    id_trabajador VARCHAR(5) PRIMARY KEY,
+    dni           VARCHAR(9) UNIQUE   NOT NULL,
+    nombre        VARCHAR(100)        NOT NULL,
+    apellidos     VARCHAR(100)        NOT NULL,
+    especialidad  VARCHAR(50)         NOT NULL,
+    contraseña    VARCHAR(50)         NOT NULL,
+    email         VARCHAR(150) UNIQUE NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS trabajo
+(
+    cod_trabajo   VARCHAR(5) PRIMARY KEY,
+    categoria     VARCHAR(50)  NOT NULL,
+    descripcion   VARCHAR(500) NOT NULL,
+    fec_ini       DATE         NOT NULL,
+    fec_fin       DATE,
+    tiempo        NUMERIC(4, 1),
+    id_trabajador VARCHAR(5),
+    CONSTRAINT fk_trabajador FOREIGN KEY (id_trabajador) REFERENCES trabajador (id_trabajador)
+    );
