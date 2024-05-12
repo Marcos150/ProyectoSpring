32ee8abb58fa5aa2ea6081fa226e66fc02a7c1d1
Marcos150 <marcosruizrubio@gmail.com>
2024-05-12 12:59:17 +0200

    Implementado ExceptionHandler de trabajador y corregido un error en la navbar

---
+++ src/main/java/com/example/proyectospring/controllers/trabajador/TrabajadorExceptionHandler.java
@@ -0,0 +1,93 @@
+package com.example.proyectospring.controllers.trabajador;
+
+import com.example.proyectospring.entities.trabajador.Trabajador;
+import com.example.proyectospring.services.trabajador.impl.TrabajadorService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.lang.reflect.InvocationTargetException;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@RestControllerAdvice(assignableTypes = TrabajadorViewController.class)
+public class TrabajadorExceptionHandler {
+    private final TrabajadorService service;
+    private Trabajador trabajador;
+
+    public TrabajadorExceptionHandler(TrabajadorService service) {
+        this.service = service;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Object handleInvalidArgument(MethodArgumentNotValidException exception, HttpServletRequest req) {
+        if (req.getRequestURI().startsWith("/api/trabajador")) {
+            Map<String, String> errorMap = new HashMap<>();
+            errorMap.put("message", "model validation failed");
+            exception.getBindingResult().getFieldErrors().forEach(error ->
+                    errorMap.put(error.getField(), error.getDefaultMessage()));
+            exception.getGlobalErrors().forEach(error -> errorMap.put(error.getCode(), error.getDefaultMessage()));
+            return errorMap;
+        } else if (req.getRequestURI().startsWith("/app/trabajador")) {
+            trabajador = new Trabajador();
+            Arrays.stream(Trabajador.class.getDeclaredFields()).forEach(field -> {
+                try {
+                    Arrays.stream(Trabajador.class.getMethods()).forEach(m -> {
+                        if (m.getName().matches("set(?i)(" + field.getName() + ")")) {
+                            try {
+                                if (field.getType().getName().equals(LocalDate.class.getTypeName())) {
+                                    m.invoke(trabajador, LocalDate.parse(req.getParameter(field.getName())));
+                                } else if (field.getType().getName().equals(BigDecimal.class.getTypeName())) {
+                                    m.invoke(trabajador, new BigDecimal(req.getParameter(field.getName())));
+                                } else {
+                                    m.invoke(trabajador, req.getParameter(field.getName()));
+                                }
+                            } catch (IllegalAccessException | DateTimeParseException | InvocationTargetException |
+                                     IllegalArgumentException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                } catch (IllegalArgumentException | SecurityException e) {
+                    e.printStackTrace();
+                }
+            });
+            return discriminateURL(req, exception);
+        }
+        return null;
+    }
+
+    private ModelAndView mvBuilder(String uri, MethodArgumentNotValidException exception) {
+        ModelAndView model = new ModelAndView(uri);
+        if (uri.contains("index")) model.addObject("trabajadores", service.getAllTrabajadores());
+        exception.getBindingResult().getFieldErrors().forEach(result -> model.addObject(result.getField(), result.getDefaultMessage()));
+        //Hardcoded
+        //exception.getGlobalErrors().forEach(result-> model.addObject("dateRange", result.getDefaultMessage()));
+        model.addObject("trabajador", trabajador);
+        model.addObject("error", "Error validando el formulario");
+        return model;
+    }
+
+    private ModelAndView discriminateURL(HttpServletRequest req, MethodArgumentNotValidException exception) {
+        Pattern pattern = Pattern.compile("/app/trabajador/put/([A-Z]|[0-9]){1,5}/?", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(req.getRequestURI());
+        if (matcher.find()) {
+            return mvBuilder("trabajador/detail", exception);
+        } else if (req.getRequestURI().startsWith("/app/trabajador/create") ||
+                req.getRequestURI().startsWith("/app/trabajador/create/")) {
+            return mvBuilder("trabajador/index", exception);
+        }
+        return mvBuilder("trabajador/detail", exception);
+    }
+}
--- src/main/java/com/example/proyectospring/entities/trabajador/Trabajador.java
+++ src/main/java/com/example/proyectospring/entities/trabajador/Trabajador.java
@@ -4,6 +4,9 @@ import com.example.proyectospring.entities.trabajo.Trabajo;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -14,24 +17,45 @@ public class Trabajador implements java.io.Serializable
 {
     @Id
     @Column(name = "id_trabajador", nullable = false, length = 5)
+    @Size(max = 5, message = "La longitud máxima del id del trabajador es de 5 caracteres")
+    @NotNull(message = "No puede ser nulo")
+    @NotEmpty(message = "El campo id trabajador no puede estar vacío")
     private String idTrabajador;
 
     @Column(name = "dni", nullable = false, length = 9)
+    @Size(max = 9, min = 9, message = "La longitud del dni es de 9 caracteres")
+    @NotNull(message = "No puede ser nulo")
+    @NotEmpty(message = "El campo dni no puede estar vacío")
     private String dni;
 
     @Column(name = "nombre", nullable = false, length = 100)
+    @Size(max = 100, message = "La longitud máxima del nombre es de 100 caracteres")
+    @NotNull(message = "No puede ser nulo")
+    @NotEmpty(message = "El campo nombre no puede estar vacío")
     private String nombre;
 
     @Column(name = "apellidos", nullable = false, length = 100)
+    @Size(max = 100, message = "La longitud máxima de los apellidos es de 100 caracteres")
+    @NotNull(message = "No puede ser nulo")
+    @NotEmpty(message = "El campo apellidos no puede estar vacío")
     private String apellidos;
 
     @Column(name = "especialidad", nullable = false, length = 50)
+    @Size(max = 100, message = "La longitud máxima de la especialidad es de 100 caracteres")
+    @NotNull(message = "No puede ser nulo")
+    @NotEmpty(message = "El campo especialidad no puede estar vacío")
     private String especialidad;
 
     @Column(name = "contrasenya", nullable = false, length = 50)
+    @Size(max = 50, message = "La longitud máxima de la contraseña es de 50 caracteres")
+    @NotNull(message = "No puede ser nulo")
+    @NotEmpty(message = "El campo contraseña no puede estar vacío")
     private String contrasenya;
 
     @Column(name = "email", nullable = false, length = 150)
+    @Size(max = 100, message = "La longitud máxima del email es de 100 caracteres") //TODO: Realmente la longitud maxima de un correo es de 255 chars
+    @NotNull(message = "No puede ser nulo")
+    @NotEmpty(message = "El campo email no puede estar vacío")
     private String email;
     @OneToMany(mappedBy = "idTrabajador",fetch = FetchType.LAZY)
     @JsonManagedReference
